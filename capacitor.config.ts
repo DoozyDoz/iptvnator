61edbbbb580fa7916715ed00a2e97a014cb85be3
Doozy <98877504+DoozyDoz@users.noreply.github.com>
2026-01-12 15:52:05 +0300

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fourgray.iptvnator',
+  appName: 'IPTVnator',
+  webDir: 'dist/browser'
+};
+
+export default config;
